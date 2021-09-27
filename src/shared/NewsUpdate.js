06559768803b96eee86ec21f6c7775db5461e476
Arun <arunkumar.s@greatinnovus.com>
2021-09-27 15:35:40 +0530

    🐛 : Fix: News and Updates container issues.

--- src/shared/NewsUpdate.js
+++ src/shared/NewsUpdate.js
@@ -30,8 +30,7 @@ const useStyles = makeStyles((theme) => ({
             marginLeft: '0',
             paddingLeft: '0',
             borderLeft: 'none',
-            padding: '12px 0',
-            height: '210px',
+            height: '250px',
             overflowX: 'unset'
         }
     }
@@ -40,12 +39,9 @@ function NewsUpdate(props) {
     const classes = useStyles();
     const [newsData, setNewsData] = useState('');
     const { t, i18n } = useTranslation('common');
-    // const [showMore, setShowMore] = useState(true);
-    const [modalShow, setModalShow] = React.useState(false);
 
     // reset login status
     useEffect(async () => {
-        //dispatch(userActions.logout()); 
         const getNewsData = await NewsService.getNewsBullet();
         if (getNewsData && getNewsData != undefined) {
             setNewsData(getNewsData['news']);
@@ -56,30 +52,20 @@ function NewsUpdate(props) {
         if (myElement) {
             const height = myElement.clientHeight;
             if (height > 195) {
-                // setShowMore(true);
                 document.getElementById("newsUpdateDiv").className = props.isMostUsedPanel ? "newsUpdateContent" : "p-3";
             }
         }
 
     }, []);
-    const elementContent = <div><p className={"subHeading" + ' ' + (props.isMostUsedPanel ? 'ml-2 mt-4' : '')}>2021 Jan 21 <b>Patent Family Updates – </b>The GenomeQuest content team has streamlined the patent family assignment process to increase accuracy and increase update frequency. The new process has resulted in a significant reduction of singletons, updates for newdocuments are now done weekly. Please contact support@gqlifesciences.com with questions or comments.</p>
-        <p className={"subHeading" + ' ' + (props.isMostUsedPanel ? 'ml-2 ' : '')}>2020 Oct 27 <b>Antibody Module – </b>The new module has been released! Please join us in a <a href='#' onClick={e => e.preventDefault()}>webinar</a> on November 10th to learn about streamlining your antibody searches.</p></div>
+
     return (
         <div>
             <div className={classes.newsContent + ' ' + (props.isMostUsedPanel ? classes.newsMostUsedContent : '') + (props.isForgotPanel ? classes.forgotNewsContent : '')}>
                 <div id="newsUpdateDiv" className="p-3">
-                    <h5 className="subHeading"><b>{t('newsandupdates')}</b></h5>
+                    <h5 className="subHeading" style={{ marginBottom: '6px' }}><b>{t('newsandupdates')}</b></h5>
                     {ReactHtmlParser(newsData)}
-                    {/* <div dangerouslySetInnerHTML={{__html: newsData}}>
-
-            </div> */}
                 </div>
             </div>
-
-            {/* {props.isMostUsedPanel && showMore &&
-                <p className="moreLink ml-2"><a className="cursorPointer appLink" onClick={() => setModalShow(true)}>More...</a></p>
-            } */}
-            <ContentModal show={modalShow} onHide={() => setModalShow(false)} title={t('newsandupdates')} contentdata={newsData} />
         </div>
     );
 }
