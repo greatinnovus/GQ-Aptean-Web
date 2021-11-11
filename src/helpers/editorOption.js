bb59d630273bb289d144ad38377072f71a542ef9
manojbabuks <manojbabu.s@greatinnovus.com>
2021-11-11 20:04:27 +0530

    Missed file Code Commit.

---
+++ src/helpers/editorOption.js
@@ -0,0 +1,25 @@
+import List from '@editorjs/list'
+// import Warning from '@editorjs/warning'
+// import Code from '@editorjs/code'
+// import LinkTool from '@editorjs/link'
+// import Image from '@editorjs/image'
+// import Raw from '@editorjs/raw'
+// import Header from '@editorjs/header'
+// import Quote from '@editorjs/quote'
+// import Marker from '@editorjs/marker'
+// import Delimiter from '@editorjs/delimiter'
+// import InlineCode from '@editorjs/inline-code'
+// import SimpleImage from '@editorjs/simple-image'
+
+export const EDITOR_JS_TOOLS = {
+  list: List,
+  // warning: Warning,
+  // code: Code,
+  // image: Image,
+  // header: Header,
+  // quote: Quote,
+  // checklist: CheckList,
+  // delimiter: Delimiter,
+  // inlineCode: InlineCode,
+  // simpleImage: SimpleImage,
+}
